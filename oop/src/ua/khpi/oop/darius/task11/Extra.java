43aad6fa230b27ac87faab8a0a1933c0f89727cb
unknown <Darius.Sudeiko@cs.khpi.edu.ua>
2024-05-17 12:31:13 +0300

    fixed lab11

--- oop/src/ua/khpi/oop/darius/task11/Extra.java
+++ oop/src/ua/khpi/oop/darius/task11/Extra.java
@@ -2,19 +2,19 @@ package ua.khpi.oop.darius.task11;
 
 public class Extra {
 	public static void main(String[] args) {
-		Pair<String> twoNames = new Pair<>("Juan", "Juanita");
-		System.out.printf("twoNames=%s.%n", twoNames);
-		String nameA = twoNames.getA(); // Value is "Juan"
-		String nameB = twoNames.getB(); // Value is "Juanita"
-		System.out.printf(" First item: %s.%n", nameA);
-		System.out.printf(" Second item: %s.%n", nameB);
+		String A = "Juan";
+		String B = "Juanita";
+		double ABalance = 2345678.99;
+		double BBalance = 15455.26;
 
-		Pair<Integer> twoNums = new Pair<>(11, 22);
-		System.out.printf("twoNums=%s.%n", twoNums);
-		Integer numA = twoNums.getA(); // Value is 11
-		Integer numB = twoNums.getB(); // Value is 22
-		System.out.printf(" First item: %s.%n", numA);
-		System.out.printf(" Second item: %s.%n", numB);
+		System.out.printf("%8s", A);
+		System.out.print("'s bank account balance is $");
+		System.out.printf("%10.2f \n", ABalance);
+		System.out.printf("%8s", B);
+		System.out.print("'s bank account balance is $");
+		System.out.printf("%10.2f ", BBalance);
 	}
 
+	// DecimalFormat formatter = new DecimalFormat("#,##0.00");
+	// String formattedBalance = formatter.format(balance);
 }
